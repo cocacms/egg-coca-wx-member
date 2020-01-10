b5f687d04fcc56868e3f728955b542b811eae039
rojer <rojerchen@qq.com>
2020-01-10 19:34:27 +0800

    fix bug

--- app/middleware/wx_member.js
+++ app/middleware/wx_member.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 module.exports = (need_login = true) => {
   return async function user(ctx, next) {
     let memberId = null;
 
-    const Authorization = ctx.get('Authorization');
+    const Authorization = ctx.get("Authorization");
     if (Authorization) {
       try {
-        ctx.service.user.verify(Authorization.replace('Basic ', ''));
+        ctx.service.user.verify(Authorization.replace("Basic ", ""));
       } catch (error) {
-        ctx.throw(401, '验证失败/过期，请重新登录');
+        ctx.throw(401, "验证失败/过期，请重新登录");
       }
       const payload = ctx.service.user.decode(
-        Authorization.replace('Basic ', '')
+        Authorization.replace("Basic ", "")
       );
       memberId = payload.id;
     }
 
     if (!memberId && need_login) {
-      ctx.throw(401, '请先登录');
+      ctx.throw(401, "请先登录");
     }
 
     if (memberId) {
-      ctx.member = await ctx.app.model.Member.findOne({
-        where: { id: memberId },
+      ctx.member = await ctx.app.model.WxMember.findOne({
+        where: { id: memberId }
       });
     }
 
